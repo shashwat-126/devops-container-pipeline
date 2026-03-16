e5c33499e1047439f5f005ef0377080473b90ff8
shashwat-126 <vibhanshashwat@gmail.com>
2026-03-17 01:08:27 +0530

    failure experiment

--- test_calculator.py
+++ test_calculator.py
@@ -2,7 +2,7 @@ from calculator import add, multiply, divide
 import pytest
 
 def test_add():
-    assert add(2,3) == 5
+    assert add(2,3) == 10
 
 def test_multiply():
     assert multiply(3,4) == 12
